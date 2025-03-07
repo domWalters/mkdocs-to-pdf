b81a3c778b1a6362b22bbe1da5d6400f1aeeb31a
Dom <waltersdom@gmail.com>
2025-03-07 16:40:51 +0000

    feat: move to one makefile that build everything, and one .venv

--- samples/material/generate-requirements-full.sh
+++ samples/material/generate-requirements-full.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source .venv/bin/activate
-pip freeze | sed "s/.*mkdocs-to-pdf.*/\.\.\/\.\.\/\.\.\//g" > requirements-full.txt
+pip freeze | sed "s/.*mkdocs-to-pdf.*/\.\.\/\.\.\//g" > requirements-full.txt
