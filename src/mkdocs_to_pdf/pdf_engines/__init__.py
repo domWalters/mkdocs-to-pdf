f5fa6633c1e46d68fde004ea61d0dbe606720d23
Hideyuki KATO <hideyuki@kato.jp>
2025-03-16 17:55:55 +0900

    fix: runtime module import

--- src/mkdocs_to_pdf/pdf_engines/__init__.py
+++ src/mkdocs_to_pdf/pdf_engines/__init__.py
@@ -1,13 +1,13 @@
 from .base import PDFEngine
-from .weasyprint import WeasyPrint
-from .chromium import Chromium
 
 
 def get_pdf_engine(engine: str) -> 'PDFEngine':
     """Get PDF engine."""
     if engine == 'weasyprint':
+        from .weasyprint import WeasyPrint
         engine = WeasyPrint()
     elif engine == 'chromium':
+        from .chromium import Chromium
         engine = Chromium()
     else:
         raise ValueError(f'PDF engine {engine} is not supported.')
