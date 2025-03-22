8d9dadb0d22debeee1796e3b38615a145d024eef
Hideyuki KATO <hideyuki@kato.jp>
2025-03-22 20:04:57 +0900

    feat: add Plarywright Chromium PDF engine

--- src/mkdocs_to_pdf/pdf_engines/chromium.py
+++ src/mkdocs_to_pdf/pdf_engines/chromium.py
@@ -2,6 +2,8 @@ import pathlib
 import tempfile
 from logging import getLogger
 
+from playwright.sync_api import sync_playwright
+
 from .base import PDFEngine
 
 logger = getLogger(__name__)
@@ -17,17 +19,25 @@ class Chromium(PDFEngine):
     def write_pdf(self, html_string: str, output_path: str):
         """Write PDF file from HTML string."""
         with tempfile.NamedTemporaryFile(
-                    mode='w', suffix='.html',
-                    dir=pathlib.Path(output_path).parent) as temp:
-            temp.write(html_string)
-            temp_path = pathlib.Path(temp.name).as_uri()
-            self.options._logger.info(f'Output a HTML to "{temp_path}"')
+            mode='w', suffix='.html', dir=pathlib.Path(output_path).parent
+        ) as html_file:
+            html_file.write(html_string)
+            html_url = pathlib.Path(html_file.name).as_uri()
+            self.options._logger.info(f'Output a HTML to "{html_url}"')
 
-        chrome_path = self.options.headless_chrome_path
-        self.options._logger.info(f'Browser executable path: {chrome_path}')
+            chrome_path = self.options.headless_chrome_path
+            self.options._logger.info(f'Browser executable: {chrome_path}')
 
-        # TODO:
-        raise NotImplementedError
+            # Rendering PDF using Playwright
+            with sync_playwright() as p:
+                if chrome_path:
+                    browser = p.chromium.launch(executable_path=chrome_path)
+                else:
+                    browser = p.chromium.launch()
+                page = browser.new_page()
+                page.goto(html_url)
+                page.pdf(path=output_path)
+                browser.close()
 
     def get_logger(self):
         """Get logger."""
