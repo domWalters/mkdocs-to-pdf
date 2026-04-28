67c5bc29a2ed6955dc9ca4a0e79033f0ff841ffe
Dom <waltersdom@gmail.com>
2026-04-28 17:33:30 +0100

    chore: fix shell.nix

--- shell.nix
+++ shell.nix
@@ -1,40 +1,30 @@
 { pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+    buildInputs = with pkgs.buildPackages; [
+        fontconfig
+        gcc
+        glib
+        gobject-introspection
+        pango
 
-(pkgs.buildFHSEnv {
-    name = "mkdocs-to-pdf-env";
-    targetPkgs = pkgs: (with pkgs; [
-        fontconfig
-        gcc
-        glib
         gnumake
-        gnumake
-        harfbuzz
         noto-fonts-cjk-sans
-        pango
-        python312
-        python312Packages.uv
-        python312Packages.uv-build
-    ]);
-    multiPkgs = pkgs: (with pkgs; [
-        fontconfig
-        gcc
-        glib
-        gnumake
-        gnumake
-        harfbuzz
-        noto-fonts-cjk-sans
-        pango
-        python312
-        python312Packages.uv
-        python312Packages.uv-build
-    ]);
-    runScript = ''
-        bash --init-file <(
-            cat $HOME/.bashrc;
-            if [ ! -f .venv/bin/activate ]; then
-                make sync;
-            fi;
-            cat .venv/bin/activate;
-        ) -i
+        pkg-config
+        python310
+        python310Packages.uv
+        python310Packages.uv-build
+    ];
+    shellHook = ''
+        if [ -f .venv/bin/activate ]; then
+            source $HOME/.bashrc;
+        fi;
+        if [ ! -f .venv/bin/activate ]; then
+            make sync;
+        fi;
+        source .venv/bin/activate;
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.fontconfig.lib}/lib;
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.gcc.cc.lib}/lib;
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.glib.out}/lib;
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.pango.out}/lib;
     '';
-}).env
+}
