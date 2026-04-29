818baa0463310110da346407baac7c410166781d
Dom <waltersdom@gmail.com>
2026-04-29 20:04:32 +0100

    fix: install fonts correctly in shell.nix
    `make docs` finally works

--- shell.nix
+++ shell.nix
@@ -1,28 +1,35 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-    buildInputs = with pkgs.buildPackages; [
-        fontconfig
-        gcc
-        glib
-        gobject-introspection
-        pango
+let
+    fontsConf = pkgs.makeFontsConf {
+        fontDirectories = [
+            pkgs.noto-fonts-cjk-sans
+        ];
+    };
+in
+    pkgs.mkShell {
+        packages = with pkgs; [
+            fontconfig
+            gcc
+            glib
+            gobject-introspection
+            pango
 
-        gnumake
-        noto-fonts-cjk-sans
-        pkg-config
-        python310
-        python310Packages.uv
-        python310Packages.uv-build
-    ];
-    shellHook = ''
-        if [ -f $HOME/.bashrc ]; then
-            source $HOME/.bashrc;
-        fi;
-        make sync;
-        source .venv/bin/activate;
-        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.fontconfig.lib}/lib;
-        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.gcc.cc.lib}/lib;
-        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.glib.out}/lib;
-        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.pango.out}/lib;
-    '';
-}
+            gnumake
+            pkg-config
+            python310
+            python310Packages.uv
+            python310Packages.uv-build
+        ];
+        shellHook = ''
+            if [ -f $HOME/.bashrc ]; then
+                source $HOME/.bashrc;
+            fi;
+            make sync;
+            source .venv/bin/activate;
+            export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.fontconfig.lib}/lib;
+            export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.gcc.cc.lib}/lib;
+            export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.glib.out}/lib;
+            export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.pango.out}/lib;
+            export FONTCONFIG_FILE="${fontsConf}"
+        '';
+    }
